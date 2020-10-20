3ebb94962b77c97188f22714f34538cd75985e50
Girish Mahajan <girish.mahajan@aiocdawacs.com>
2020-10-20 19:42:43 +0530

    first commit

--- src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/controller/ProductController.java
+++ src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/controller/ProductController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +29,11 @@ public class ProductController {
     @GetMapping( path = "/add")
     public void saveProduct(@RequestBody Product productToSave){
         productRepository.save(productToSave);
+    }
+    
+    @GetMapping( path = "/{id}")
+    public Product getProduct(@PathVariable Long id){
+    	System.out.println("searching product for "+id);
+        return productRepository.findById(id).orElse(null);
     }
 }
