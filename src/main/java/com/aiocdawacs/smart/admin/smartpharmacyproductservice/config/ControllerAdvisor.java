219544d6b66cdd39583bc085203520a1513bb9ce
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-10-24 20:27:58 +0530

    foo

--- src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/config/ControllerAdvisor.java
+++ src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/config/ControllerAdvisor.java
@@ -7,12 +7,17 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.client.HttpClientErrorException.Unauthorized;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -22,6 +27,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(ControllerAdvisor.class);
 
 	@ExceptionHandler(ProductorderNotFoundException.class)
 	public ResponseEntity<Object> handleProductorderNotFoundException(
@@ -67,5 +73,23 @@ public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 		body.put("errors", errors);
 
 		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
+
+
+	@Autowired
+	private ApplicationEventPublisher publisher;
+
+	@ExceptionHandler(value = { Unauthorized.class })
+	public ResponseEntity<Object> handleUnauthorizedException(Unauthorized ex) {
+		LOGGER.error("Unauthorized Exception: ", ex.getMessage());
+		publisher.publishEvent(new String("unauthorized access"));
+		return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+	}
+
+	@ExceptionHandler(value = { Exception.class })
+	public ResponseEntity<Object> handleException(Exception ex) {
+		LOGGER.error("Exception: ", ex.getMessage());
+		publisher.publishEvent(new String("error occured in the smart pharmacy :" + ex.getMessage()));
+		return new ResponseEntity<Object>(ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
