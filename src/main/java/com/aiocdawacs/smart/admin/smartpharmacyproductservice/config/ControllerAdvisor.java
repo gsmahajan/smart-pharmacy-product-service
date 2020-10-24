4906c3b9332caf5ac1cbfffe72e81079a7b22f9a
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-10-24 19:54:02 +0530

    typo correction

--- src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/config/ControllerAdvisor.java
+++ src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/config/ControllerAdvisor.java
@@ -1,6 +1,5 @@
 package com.aiocdawacs.smart.admin.smartpharmacyproductservice.config;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.LinkedHashMap;
@@ -15,53 +14,58 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 
 @ControllerAdvice
 public class ControllerAdvisor extends ResponseEntityExceptionHandler {
 
-	
-	
-    @ExceptionHandler(ProductorderNotFoundException.class)
-    public ResponseEntity<Object> handleCityNotFoundException(
-    		ProductorderNotFoundException ex, WebRequest request) {
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
-        body.put("message", ex.getMessage());
-        body.put("poweredBy", "AwacsInternational");
-        
-        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
-    }
 
-	/*
-	 * @ExceptionHandler(NoDataFoundException.class) public ResponseEntity<Object>
-	 * handleNodataFoundException( NoDataFoundException ex, WebRequest request) {
-	 * 
-	 * Map<String, Object> body = new LinkedHashMap<>(); body.put("timestamp",
-	 * LocalDateTime.now()); body.put("message", "No cities found");
-	 * 
-	 * return new ResponseEntity<>(body, HttpStatus.NOT_FOUND); }
-	 */
+	@ExceptionHandler(ProductorderNotFoundException.class)
+	public ResponseEntity<Object> handleProductorderNotFoundException(
+			ProductorderNotFoundException ex, WebRequest request) {
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-        MethodArgumentNotValidException ex, HttpHeaders headers, 
-        HttpStatus status, WebRequest request) {
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+		body.put("message", ex.getMessage());
+		body.put("x-powered-by", "AwacsInternational");
 
-        Map<String, Object> body = new LinkedHashMap<>();
-        body.put("timestamp", LocalDate.now());
-        body.put("status", status.value());
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
 
-        List<String> errors = ex.getBindingResult()
-                .getFieldErrors()
-                .stream()
-                .map(x -> x.getDefaultMessage())
-                .collect(Collectors.toList());
+	@ExceptionHandler(MethodArgumentTypeMismatchException.class)
+	public ResponseEntity<Object> handleNumberformatException(
+			MethodArgumentTypeMismatchException ex, WebRequest request) {
 
-        body.put("errors", errors);
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+		body.put("message", ex.getMessage());
+		body.put("x-powered-by", "AwacsInternational");
 
-        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
-    }
+		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, 
+			HttpStatus status, WebRequest request) {
+
+		Map<String, Object> body = new LinkedHashMap<>();
+		body.put("timestamp", LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS));
+		body.put("message", ex.getMessage());
+		body.put("status", status.value());
+		body.put("x-powered-by", "AwacsInternational");
+
+		List<String> errors = ex.getBindingResult()
+				.getFieldErrors()
+				.stream()
+				.map(x -> x.getDefaultMessage())
+				.collect(Collectors.toList());
+
+		body.put("errors", errors);
+
+		return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+	}
 }
