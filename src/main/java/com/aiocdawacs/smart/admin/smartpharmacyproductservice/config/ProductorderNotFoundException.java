5388078f448602c56180944729bd023b1243f306
girishaiocdawacs <girish.mahajan@aiocdawacs.com>
2020-10-24 19:37:45 +0530

    typo correction

---
+++ src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/config/ProductorderNotFoundException.java
@@ -0,0 +1,10 @@
+package com.aiocdawacs.smart.admin.smartpharmacyproductservice.config;
+
+public class ProductorderNotFoundException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public ProductorderNotFoundException(Long id) {
+		super(String.format("product order with id %d not found", id));
+	}
+}
--- src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/controller/ProductOrderController.java
+++ src/main/java/com/aiocdawacs/smart/admin/smartpharmacyproductservice/controller/ProductOrderController.java
@@ -1,3 +1,4 @@
+
 package com.aiocdawacs.smart.admin.smartpharmacyproductservice.controller;
 
 import java.util.Arrays;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.aiocdawacs.smart.admin.smartpharmacyproductservice.config.ProductorderNotFoundException;
 import com.aiocdawacs.smart.admin.smartpharmacyproductservice.model.ProductOrder;
 import com.aiocdawacs.smart.admin.smartpharmacyproductservice.repository.ProductOrderRepository;
 
@@ -29,8 +31,9 @@ public class ProductOrderController {
 	}
 
 	@GetMapping("/{orderId}")
-	public ResponseEntity<ProductOrder> getOrder(@PathVariable Long orderId ){
-		return ResponseEntity.of(Optional.ofNullable(this.productOrderRepository.findById(orderId).orElse(null)));
+	public ResponseEntity<ProductOrder> getOrder(@PathVariable Long orderId ) throws ProductorderNotFoundException {
+		return ResponseEntity.of(Optional.ofNullable(this.productOrderRepository.findById(orderId)
+				.orElseThrow(() -> new ProductorderNotFoundException(orderId))));
 	}
 
 	@GetMapping("/all")
